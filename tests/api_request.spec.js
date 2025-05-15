f5c0efa247cb68655214caf5992f0ebdc028add0
Akombo Neville Akwo <akomboneville55@gmail.com>
2025-05-15 05:43:05 +0100

    Get booking by id

--- tests/api_request.spec.js
+++ tests/api_request.spec.js
@@ -26,7 +26,7 @@ test.describe("API request", () => {
     expect(json.length).toBeGreaterThan(0);
   });
   test.only("Test3: Get booking by id", async ({ request }) => {
-    const response = await request.get(`/booking/${714}`);
+    const response = await request.get(`/booking/714`);
     expect(response.ok()).toBeTruthy();
     const json = await response.json();
     console.log(json);
